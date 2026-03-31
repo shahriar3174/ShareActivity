e8b1691fdf1833dcd8a8b53bfc1bfcc4ca540ab6
shahriar3174 <165700935+shahriar3174@users.noreply.github.com>
2026-03-31 12:03:20 -0400

    Activity done

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +16,16 @@ class MainActivity : AppCompatActivity() {
 
         // When the user clicks this button, share the text if not empty
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
-
+            val textToShare = editText.text.toString()
+            if (textToShare.isNotEmpty()) {
+                val shareIntent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, textToShare)
+                    type = "text/plain"
+                }
+                val chooser = Intent.createChooser(shareIntent, "Share text via")
+                startActivity(chooser)
+            }
         }
     }
 }
